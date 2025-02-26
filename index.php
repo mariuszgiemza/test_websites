a3ad057fec293c9c5441d5f7853a8374486d4fd5
Mariusz Giemza <37539172+mariuszgiemza@users.noreply.github.com>
2025-02-26 20:33:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ date_default_timezone_set("Europe/Warsaw");
 <body><br><br>
     <h1>Aplikacja w PHP, hostowana w Microsoft Azure</h1>
     <br><br>
+     <h1>!!!NOWA WERSJA!!!</h1>
+    <br><br>
     <h3>Dziś jest <?php echo date("d.m.Y"); ?>, godzina <?php echo date("H:i"); ?></h3>  
 </body>
 
