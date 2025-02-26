2658ed622fde562e46bd792d6ab87c3dffd03cbd
Mariusz Giemza <37539172+mariuszgiemza@users.noreply.github.com>
2025-02-26 20:01:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     now = datetime.now().astimezone(pytz.timezone("Europe/Warsaw"))
-    return f"<h1>Aktualny czas: {now.hour}:{now.minute:0{2}} {now.day}.{now.month}.{now.year}</h1>"
+    return f"<h1><br><br><br>Aplikacja napisana w Pythonie, hostowana w Microsoft Azure. <br><br><br>Dziś jest {now.day}.{now.month}.{now.year} godzina {now.hour}:{now.minute:0{2}} </h1>"
 
 if __name__ == "__main__":
     app.run()
