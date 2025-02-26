932440b4d300960aa03ca75cd069e92cef821943
Mariusz Giemza <37539172+mariuszgiemza@users.noreply.github.com>
2025-02-26 20:11:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from datetime import datetime
-import pytz
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    now = datetime.now().astimezone(pytz.timezone("Europe/Warsaw"))
+    now = datetime.now()
     return f"<h1><br><br><br>Aplikacja napisana w Pythonie, hostowana w Microsoft Azure. <br><br><br>Dziś jest {now.day}.{now.month}.{now.year} godzina {now.hour}:{now.minute:0{2}} </h1>"
 
 if __name__ == "__main__":
