d877a6345b981a4ef57f8e5172b785bd0103fe51
MarceliGG <marcel.giemza@outlook.com>
2025-02-26 19:54:15 +0100

    update app.py to use warsaw timezone

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    now = datetime.now()
+    now = datetime.now().astimezone(pytz.timezone("Europe/Warsaw"))
     return f"<h1>Aktualny czas: {now.hour}:{now.minute:0{2}} {now.day}.{now.month}.{now.year}</h1>"
 
 if __name__ == "__main__":
